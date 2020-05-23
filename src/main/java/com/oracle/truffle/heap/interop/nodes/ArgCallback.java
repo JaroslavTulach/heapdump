0563a920a678940b4106a922c49b90203dc5a4d8
Samuel Pastva <sam.pastva@gmail.com>
2020-05-23 17:31:35 +0200

    fool-proof ArgCallback node

--- src/main/java/com/oracle/truffle/heap/interop/nodes/ArgCallback.java
+++ src/main/java/com/oracle/truffle/heap/interop/nodes/ArgCallback.java
@@ -2,6 +2,7 @@ package com.oracle.truffle.heap.interop.nodes;
 
 import com.oracle.truffle.api.dsl.Cached;
 import com.oracle.truffle.api.dsl.GenerateUncached;
+import com.oracle.truffle.api.dsl.ImportStatic;
 import com.oracle.truffle.api.dsl.Specialization;
 import com.oracle.truffle.api.interop.InteropLibrary;
 import com.oracle.truffle.api.interop.TruffleObject;
@@ -9,6 +10,7 @@ import com.oracle.truffle.api.interop.UnsupportedTypeException;
 import com.oracle.truffle.api.library.CachedLibrary;
 import com.oracle.truffle.api.nodes.Node;
 import com.oracle.truffle.heap.HeapLanguage;
+import com.oracle.truffle.heap.interop.Args;
 import com.oracle.truffle.heap.interop.Errors;
 import com.oracle.truffle.heap.interop.Types;
 
@@ -16,6 +18,7 @@ import com.oracle.truffle.heap.interop.Types;
  * Unwrap a callback argument that can be either an executable truffle object, or a string expression.
  */
 @GenerateUncached
+@ImportStatic(Args.class)
 public abstract class ArgCallback extends Node {
     public abstract TruffleObject execute(Object[] arguments, int index, String[] argNames) throws UnsupportedTypeException;
 
@@ -37,14 +40,19 @@ public abstract class ArgCallback extends Node {
         return HeapLanguage.parseArgumentExpression(expression, argNames);
     }
 
-    @Specialization(guards = "isExecutable(arguments, index, interop)")
+    @Specialization(guards = "isMissing(arguments, index)") @SuppressWarnings("unused")
+    static TruffleObject doMissing(Object[] arguments, int index, String[] argNames) {
+        return null;
+    }
+
+    @Specialization(guards = { "!isMissing(arguments, index)", "isExecutable(arguments, index, interop)" }) @SuppressWarnings("unused")
     static TruffleObject doExecutable(Object[] arguments, int index, String[] argNames,
                                       @CachedLibrary(limit = "3") InteropLibrary interop
     ) {
         return (TruffleObject) arguments[index];
     }
 
-    @Specialization(guards = { "isString(arguments, index, interop)", "expression.equals(expression(arguments, index, interop))" })
+    @Specialization(guards = { "!isMissing(arguments, index)", "isString(arguments, index, interop)", "expression.equals(expression(arguments, index, interop))" })
     static TruffleObject doExpressionCached(Object[] arguments, int index, String[] argNames,
                                             @CachedLibrary(limit = "3") InteropLibrary interop,
                                             @Cached("expression(arguments, index, interop)") String expression,
@@ -57,7 +65,20 @@ public abstract class ArgCallback extends Node {
     static TruffleObject doGeneric(Object[] arguments, int index, String[] argNames,
                                    @CachedLibrary(limit = "3") InteropLibrary interop
     ) throws UnsupportedTypeException {
-        return Errors.unsupportedType(arguments, "TODO");
+        Object arg = (index >= arguments.length) ? null : arguments[index];
+        if (arg == null) {
+            return null;
+        } else if (arg instanceof String) {
+            return parseExpression((String) arg, argNames);
+        } else if (interop.isExecutable(arg)) {
+            return (TruffleObject) arg;
+        } else if (interop.isString(arg)) {
+            return parseExpression(Types.asString(arg, interop), argNames);
+        } else if (interop.isNull(arg)) {
+            return null;
+        } else {
+            return Errors.unsupportedType(arguments, "Expected executable/string expression as argument %d, but found %s.", index+1, arg);
+        }
     }
 
 }
