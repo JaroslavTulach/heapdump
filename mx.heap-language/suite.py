3d8f6e583e83daa47871f8b066bc047db1052d97
Jaroslav Tulach <jaroslav.tulach@oracle.com>
2020-06-08 18:34:55 +0200

    Explicitly using compiler from GraalVM 20.1

--- mx.heap-language/suite.py
+++ mx.heap-language/suite.py
@@ -1,4 +1,17 @@
 suite = {
-  "mxversion" : "5.264.3",
-  "name" : "heap-language"
+    "mxversion" : "5.264.3",
+    "name" : "heap-language",
+    "imports": {
+        "suites": [
+            {
+                "name": "compiler",
+                "subdir": True,
+                "version": "9372629860c9768db70d4f0657dba39bf1d97710",
+                "urls": [
+                    {"url": "https://github.com/oracle/graal.git", "kind": "git"},
+                    {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
+                ]
+            },
+        ]
+    }, 
 }
